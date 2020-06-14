ed152ef7277f60a8d3b5a82f5781b5d1fc41479a
bajohw87 <66767513+bajohw87@users.noreply.github.com>
2020-06-14 12:06:21 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,18 @@
 import discord
-#import pymongo
-#import datetime
+import pymongo
+import datetime
+import os
 
 #discord client
 client = discord.Client()
 
 #heroku mongodb
-#connection = pymongo.MongoClient('mongodb://%s:%s@ds237735.mlab.com:37735/heroku_0fj18lf0?retryWrites=false' % (os.environ["DB_USERNAME"], os.environ["DB_PASSWORD"]))
-#db = connection["heroku_0fj18lf0"]
-#fixCol = db["FIX_MEMBERS"]
-#rCol = db["R_MEMBERS"]
-#jobArr = ["전사","도적","법사","흑마","냥꾼","사제","술사","드루"]
-#roleArr = ["탱","딜","힐"]
+connection = pymongo.MongoClient('mongodb://%s:%s@ds237735.mlab.com:37735/heroku_0fj18lf0?retryWrites=false' % (os.environ["DB_USERNAME"], os.environ["DB_PASSWORD"]))
+db = connection["heroku_0fj18lf0"]
+fixCol = db["FIX_MEMBERS"]
+rCol = db["R_MEMBERS"]
+jobArr = ["전사","도적","법사","흑마","냥꾼","사제","술사","드루"]
+roleArr = ["탱","딜","힐"]
 
 #참고 DB query Result
 #insert -> result.inserted_id or result.inserted_ids
